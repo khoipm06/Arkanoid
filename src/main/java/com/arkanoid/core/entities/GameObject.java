6a90565d864f6db67035f5dbdcbd7c7478868033
Khoi Pham Minh <khoipm06@gmail.com>
2025-10-01 10:05:32 +0700

    feat: add GameObject base class

---
+++ src/main/java/com/arkanoid/core/entities/GameObject.java
@@ -0,0 +1,44 @@
+package com.arkanoid.core.entities;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public abstract class GameObject {
+    protected double x;
+    protected double y;
+    protected double width;
+    protected double height;
+    protected boolean active;
+
+    public GameObject(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.active = true;
+    }
+
+    public abstract void update(double deltaTime);
+    public abstract void render(GraphicsContext gc);
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
+    public boolean isActive() { return active; }
+    
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+    public void setWidth(double width) { this.width = width; }
+    public void setHeight(double height) { this.height = height; }
+    public void setActive(boolean active) { this.active = active; }
+
+    public double getCenterX() { return x + width / 2; }
+    public double getCenterY() { return y + height / 2; }
+
+    public boolean intersects(GameObject other) {
+        return x < other.x + other.width &&
+               x + width > other.x &&
+               y < other.y + other.height &&
+               y + height > other.y;
+    }
+}
