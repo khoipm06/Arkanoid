a1fa361a2d4df4bafd749155866a320c5e831fa6
Khoi Pham Minh <khoipm06@gmail.com>
2025-10-02 15:05:26 +0700

    feat: add StrongBrick with multiple hit points

---
+++ src/main/java/com/arkanoid/core/entities/StrongBrick.java
@@ -0,0 +1,9 @@
+package com.arkanoid.core.entities;
+
+import javafx.scene.paint.Color;
+
+public class StrongBrick extends BaseBrick {
+    public StrongBrick(double x, double y, double width, double height) {
+        super(x, y, width, height, 3, Color.PURPLE);
+    }
+}
