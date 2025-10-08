675015dff1d0fa1ba8a9bfcd4706d4cd6627cb33
Khoi Pham Minh <khoipm06@gmail.com>
2025-10-08 16:48:55 +0700

    feat: add ExplosiveBallPowerUp

---
+++ src/main/java/com/arkanoid/core/entities/ExplosiveBallPowerUp.java
@@ -0,0 +1,14 @@
+package com.arkanoid.core.entities;
+
+import javafx.scene.paint.Color;
+
+public class ExplosiveBallPowerUp extends PowerUp {
+    public ExplosiveBallPowerUp(double x, double y) {
+        super(x, y, 20);
+        this.color = Color.RED;
+    }
+
+    @Override
+    public void applyEffect(Paddle paddle) {
+    }
+}
