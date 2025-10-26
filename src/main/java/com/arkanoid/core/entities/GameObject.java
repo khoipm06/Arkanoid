4ca38cc26e8cfb340c4a51103c47afe7d4ce6ee3
Khoi Pham Minh <khoipm06@gmail.com>
2025-10-26 18:57:17 +0700

    refactor: Improve formatting and readability in GameObject and LevelManager classes

--- src/main/java/com/arkanoid/core/entities/GameObject.java
+++ src/main/java/com/arkanoid/core/entities/GameObject.java
@@ -18,27 +18,61 @@ public abstract class GameObject {
     }
 
     public abstract void update(double deltaTime);
+
     public abstract void render(GraphicsContext gc);
 
-    public double getX() { return x; }
-    public double getY() { return y; }
-    public double getWidth() { return width; }
-    public double getHeight() { return height; }
-    public boolean isActive() { return active; }
-    
-    public void setX(double x) { this.x = x; }
-    public void setY(double y) { this.y = y; }
-    public void setWidth(double width) { this.width = width; }
-    public void setHeight(double height) { this.height = height; }
-    public void setActive(boolean active) { this.active = active; }
+    public double getX() {
+        return x;
+    }
 
-    public double getCenterX() { return x + width / 2; }
-    public double getCenterY() { return y + height / 2; }
+    public double getY() {
+        return y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public double getCenterX() {
+        return x + width / 2;
+    }
+
+    public double getCenterY() {
+        return y + height / 2;
+    }
 
     public boolean intersects(GameObject other) {
         return x < other.x + other.width &&
-               x + width > other.x &&
-               y < other.y + other.height &&
-               y + height > other.y;
+                x + width > other.x &&
+                y < other.y + other.height &&
+                y + height > other.y;
     }
 }
